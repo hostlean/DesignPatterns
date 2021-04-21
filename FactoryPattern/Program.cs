0525f7e69370e152880ce9d9585ec8f7a01cb5f5
hostlean <hostlean@users.noreply.github.com>
2021-04-21 14:03:03 +0300

    Initial commit

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteCreatorA creatorA = new ConcreteCreatorA();
+
+            ConcreteCreatorB creatorB = new ConcreteCreatorB();
+
+            creatorB.CreateProduct();
+
+            creatorA.CreateProduct();
+
+        }
+    }
+    
+    public abstract class Creator
+    {
+        public abstract IProduct CreateProduct();
+    }
+    
+    
+    public interface IProduct
+    {
+        public void CreatedMessage();
+    }
+    
+    
+    public class ConcreteCreatorA : Creator
+    {
+        public override IProduct CreateProduct()
+        {
+            return new ConcreteProductA();
+        }
+    }
+    
+    
+    public class ConcreteCreatorB : Creator
+    {
+        public override IProduct CreateProduct()
+        {
+            return new ConcreteProductB();
+        }
+    }
+    
+    
+    public class ConcreteProductA : IProduct
+    {
+
+        public ConcreteProductA()
+        {
+            CreatedMessage();
+        }
+        
+        public void CreatedMessage()
+        {
+            Console.WriteLine("Product A is created.");
+        }
+    }
+    
+    
+    public class ConcreteProductB : IProduct
+    {
+        public ConcreteProductB()
+        {
+            CreatedMessage();
+        }
+        
+        public void CreatedMessage()
+        {
+            Console.WriteLine("Product B is created.");
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+}
